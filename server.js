56e76941196f33aff35f04e68bf0593cdb96949b
khadija <abdix129@umn.edu>
2018-05-30 11:50:34 -0500

    myfiles

--- server.js
+++ server.js
@@ -5,8 +5,22 @@ var bodyParser = require("body-parser");
 // EXPRESS CONFIGURATION
 var app = express();
 
-// Sets an initial port.
-app.set( 'port', ( process.env.PORT || 5000 ));
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+// ejs render automatically looks in the views folder
+    res.render('index');
+});
 
 
 // Sets up the Express app to handle data parsing
@@ -19,6 +33,6 @@ require("./routes/router")(app);
 
 
 // LISTENER
-app.listen( app.get( 'port' ), function() {
-    console.log( 'Node server is running on port ' + app.get( 'port' ));
-    });
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
